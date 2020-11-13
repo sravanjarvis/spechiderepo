851931518e350c00a9f41a9a52c2a8622227efbb
sravanjarvis <68535329+sravanjarvis@users.noreply.github.com>
2020-11-13 10:27:44 +0530

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -84,9 +84,9 @@ async def upload_to_tg(
             LOGGER.info("TODO")
             d_f_s = humanbytes(os.path.getsize(local_file_name))
             i_m_s_g = await message.reply_text(
-                "Telegram does not support uploading this file.\n"
-                f"Detected File Size: {d_f_s} 😡\n"
-                "\n🤖 trying to split the files 🌝🌝🌚"
+                "Telegram does not support uploading a file more than 2GB .\n"
+                f"Detected File Size: {d_f_s} 🥺\n"
+                "\n🤖 trying to split the files ☺️☺️"
             )
             splitted_dir = await split_large_files(local_file_name)
             totlaa_sleif = os.listdir(splitted_dir)
@@ -95,9 +95,9 @@ async def upload_to_tg(
             LOGGER.info(totlaa_sleif)
             ba_se_file_name = os.path.basename(local_file_name)
             await i_m_s_g.edit_text(
-                f"Detected File Size: {d_f_s} 😡\n"
+                f"Detected File Size: {d_f_s} 🥺\n"
                 f"<code>{ba_se_file_name}</code> splitted into {number_of_files} files.\n"
-                "trying to upload to Telegram, now ..."
+                "<b><i>Uploading to Telegram, Please be patient 😅</i></b>"
             )
             for le_file in totlaa_sleif:
                 # recursion: will this FAIL somewhere?
